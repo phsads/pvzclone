f19f3bc636eec2c6f6e128bfbf0e30630082b476
phsads <64845737+phsads@users.noreply.github.com>
2022-05-26 20:23:04 +0700

    h

---
+++ src/variables.js
@@ -0,0 +1,55 @@
+
+
+
+
+//empty space for trolling
+
+
+var items = [
+	// @string name
+	"Copper Ore",
+	"Tin Ore",
+	"Copper Ingot",
+	"Tin Ingot",
+	"Copper Plate",
+	"Tin Plate",
+	"Copper Foil",
+	"Tin Foil",
+	"Copper Wire",
+	"Tin Wire",
+	"Wood",
+	"Plank",
+	"Wood Tube",
+	"Primitive Circuit Board",
+	"Primitive Diode",
+	"Primitive Circuit",
+	"Red Science Pack",
+	"Basic Machine Casing",
+	"Coal",
+	"Coal Dust",
+]
+var fluids = [
+	// @string name
+]
+var recipes = [
+	/*
+	template:
+		array:
+			[0]: array contains arrays of length 3 (recipe inputs):
+				[0]: is item?	
+				[1]: id
+				[2]: amount
+			[1]: array contains arrays of length 3 (recipe outputs):
+				[0]: is item?
+				[1]: id
+				[2]: amount
+			[2]: array contains type of machine needed (0-9999)
+	*/
+	[[[1,0,1]],[[1,2,1]],[1]],
+	[[[1,1,1]],[[1,3,1]],[1]], 
+	[[[1,2,1]],[[1,4,1]],[1]],
+	[[[1,3,1]],[[1,5,1]],[1]],
+	[[[1,4,1]],[[1,6,1]],[1]],
+	[[[1,5,1]],[[1,7,1]],[1]],
+
+]
